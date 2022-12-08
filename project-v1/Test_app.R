07c7f72f009876df12e1836a70f5de09549fc29e
jwei34 <jenny_wei@brown.edu>
2022-12-08 12:38:18 -0500

    added parameter UIs and histogram

--- project-v1/Test_app.R
+++ project-v1/Test_app.R
@@ -5,6 +5,8 @@ source(file = 'proj_simulation.R', local = TRUE)
 # Load the ggplot2 library for plotting
 library(ggplot2)
 
+seasons <- c('Spring','Summer','Fall','Winter')
+
 # Define the user interface
 ui <- fluidPage(
   
@@ -18,6 +20,12 @@ ui <- fluidPage(
       # Slider
       sliderInput("population", "Population:", min = 25000, max = 5000000, value = 25000, step = 25000), 
       
+      # Females to Males Ratio
+      sliderInput("f_m_ratio", "Female:Male Ratio", 0.5, min = 0, max = 1),
+      
+      # Season
+      selectInput("season", "Select season", seasons, "Summer"),
+      
       # Button
       actionButton("button", "Refresh plot")
     ),
@@ -26,7 +34,8 @@ ui <- fluidPage(
     mainPanel(
       
       # Output
-      plotOutput("plot")
+      plotOutput("plot1"),
+      plotOutput("plot2")
     )
   )
 )
@@ -36,7 +45,7 @@ server <- function(input, output) {
   
   # When the button is clicked, generate a plot
   observeEvent(input$button, {
-    output$plot <- renderPlot({
+    output$plot1 <- renderPlot({
       
       # Run the simulation with the specified population
       result <- data.frame(x = seq(1:100))
@@ -51,6 +60,13 @@ server <- function(input, output) {
               axis.title.x = element_text(size=12,face="bold", vjust=-3),
               axis.title.y = element_text(size=12,face="bold", vjust=7), plot.margin = margin(20, 15, 20, 25), 
               legend.title = element_text(face='bold')) 
+    })
+    output$plot2 <- renderPlot({
+      LoS_dist <- rpois(input$population, lambda = 4.6)
+      hist(LoS_dist, 
+           main = "Patients' Length of Stay",
+           ylab='# Patients',
+           xlab ='Days')
     })
   })
 }
